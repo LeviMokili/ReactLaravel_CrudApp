15f81c3a5bb1e665cbe660a9d93ef3844efc7b52
LeviMokili <levismp8d@gmail.com>
2024-03-15 18:43:06 +0300

    changes

--- my-app/src/components/Addstudent.jsx
+++ my-app/src/components/Addstudent.jsx
@@ -1,25 +1,46 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 
 function Addstudent() {
   const [count, setCount] = useState(0)
 
+
+  //GET INPUTS VALUES
+  state = {
+    name:'',
+    course:'',
+    email:'',
+    phone:'',
+  }
+
+  handleInput = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+  }
+
+  saveStudent = async (e) => {
+    e.preventDefault()
+    const res = await axios.post('/api/add-students', this.state);
+  }
+
   return (
     <>
       <div className="container">
                 <div className="col-md-12">
-                    <form action="">
+                    <form  onsubmit={this.saveStudent}>
                         <div className="form-group mb-3">
-                            <input type="text" className="form-control" name="name" placeholder='name' />
+                            <input type="text" className="form-control" name="name" placeholder='name' onChange={this.handleInput} value={this.state.name} />
                         </div>
                         <div className="form-group mb-3">
-                            <input type="text" className="form-control" name="course" placeholder="course" />
+                            <input type="text" className="form-control" name="course" placeholder="course" onChange={this.handleInput} value={this.state.course}   />
                         </div>
                         <div className="form-group mb-3">
-                            <input type="text" className="form-control" name="email" placeholder="email"  />
+                            <input type="text" className="form-control" name="email" placeholder="email"  onChange={this.handleInput}  value={this.state.email}   />
                         </div>
                         <div className="form-group mb-3">
-                            <input type="text" className="form-control"  name="phone" placeholder="phone" />
+                            <input type="text" className="form-control"  name="phone" placeholder="phone" onChange={this.handleInput}  value={this.state.phone}   />
                         </div>
                         <div className="form-group mb-3">
                             <button className="btn btn-primary" >save</button>
